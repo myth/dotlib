294c27c305fc184d87f6029a3617d2dfeb1957f5
mythern <aleks@overflow.no>
2014-05-12 00:33:50 +0200

    Added deployscript

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+VIM_CONF = "https://raw.githubusercontent.com/mythern/dotlib/master/.vimrc"
+ZSH_CONF = "https://raw.githubusercontent.com/mythern/dotlib/master/.zshrc"
+
+wget -O ~/.vimrc VIM_CONF
+wget -O ~/.zshrc ZSH_CONF
+
