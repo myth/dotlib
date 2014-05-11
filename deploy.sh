3c67e05c9df10663de9bb193ec5b0570a6d53b47
mythern <aleks@overflow.no>
2014-05-12 00:36:36 +0200

    Fixed typo

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 VIM_CONF = "https://raw.githubusercontent.com/mythern/dotlib/master/.vimrc"
 ZSH_CONF = "https://raw.githubusercontent.com/mythern/dotlib/master/.zshrc"
 
-wget -O ~/.vimrc VIM_CONF
-wget -O ~/.zshrc ZSH_CONF
+wget -O ~/.vimrc $VIM_CONF
+wget -O ~/.zshrc $ZSH_CONF
 
