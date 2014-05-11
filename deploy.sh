ac0ed85e878cf30debe2a88e70883b33a8c27c8a
mythern <aleks@overflow.no>
2014-05-12 00:46:10 +0200

    Fixer stupid error

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
-VIM_CONF = "https://raw.githubusercontent.com/mythern/dotlib/master/.vimrc"
-ZSH_CONF = "https://raw.githubusercontent.com/mythern/dotlib/master/.zshrc"
+VIM_CONF="https://raw.githubusercontent.com/mythern/dotlib/master/.vimrc"
+ZSH_CONF="https://raw.githubusercontent.com/mythern/dotlib/master/.zshrc"
 
-wget -O ~/.vimrc $VIM_CONF
-wget -O ~/.zshrc $ZSH_CONF
+wget -O ~/.vimrc -q "$VIM_CONF"
+wget -O ~/.zshrc -q "$ZSH_CONF"
 
