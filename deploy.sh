10069be9e7f504f462ce39688648fb40db020c51
myth <myth@overflow.no>
2015-11-27 01:46:13 +0100

    Add custom theme

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 VIM_CONF="https://raw.githubusercontent.com/myth/dotlib/master/.vimrc"
 ZSH_CONF="https://raw.githubusercontent.com/myth/dotlib/master/.zshrc"
+THEME="https://raw.githubusercontent.com/myth/dotlib/master/myth.zsh-theme"
 
 wget -O ~/.vimrc -q "$VIM_CONF"
 wget -O ~/.zshrc -q "$ZSH_CONF"
-
+wget -O ~/.oh-my-zsh/themes/myth.zsh-theme -q "$THEME"
