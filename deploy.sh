83bd8370a970e97547847f79b8380e93d35781ed
myth <myth@overflow.no>
2015-11-27 01:35:10 +0100

    Update deployscript

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VIM_CONF="https://raw.githubusercontent.com/mythern/dotlib/master/.vimrc"
-ZSH_CONF="https://raw.githubusercontent.com/mythern/dotlib/master/.zshrc"
+VIM_CONF="https://raw.githubusercontent.com/myth/dotlib/master/.vimrc"
+ZSH_CONF="https://raw.githubusercontent.com/myth/dotlib/master/.zshrc"
 
 wget -O ~/.vimrc -q "$VIM_CONF"
 wget -O ~/.zshrc -q "$ZSH_CONF"
